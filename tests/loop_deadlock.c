8aa907c3b4af6787ef6ae1e6b943f451736b7955
asija-p <apopovic3002@gmail.com>
2026-08-22 18:05:19 +0200

    Add loop test case confirming worklist handles CFG cycles

---
+++ tests/loop_deadlock.c
@@ -0,0 +1,21 @@
+#include <pthread.h>
+#include <stddef.h>
+
+pthread_mutex_t m1, m2;
+
+void thread1() {
+    int i;
+    for (i = 0; i < 5; i++) {
+        pthread_mutex_lock(&m1);
+        pthread_mutex_lock(&m2);
+        pthread_mutex_unlock(&m2);
+        pthread_mutex_unlock(&m1);
+    }
+}
+
+void thread2() {
+    pthread_mutex_lock(&m2);
+    pthread_mutex_lock(&m1);
+    pthread_mutex_unlock(&m1);
+    pthread_mutex_unlock(&m2);
+}

3c26515ebaeba43172fc57b4e86b63b0c199bab8
asija-p <apopovic3002@gmail.com>
2026-09-04 11:06:50 +0200

    Confirm conditional unlock is correctly handled by may/must lockset merge

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -90,6 +90,7 @@ int main(int argc, char** argv) {
         {"tests/deadlock/nested_wrapper_deadlock.c", true, {{"m1","m2"}, {"m2","m1"}}},
         {"tests/deadlock/switch_deadlock.c", true, {{"m1","m2"}, {"m2","m1"}}},
         {"tests/deadlock/disconnected_clusters_deadlock.c", true, {{"a1","a2"}, {"a2","a1"}, {"b1","b2"}, {"b2","b1"}}},
+        {"tests/deadlock/conditional_unlock_deadlock.c", true, {{"m1","m2"}, {"m2","m1"}}},
     };
 
     std::string Filter;
